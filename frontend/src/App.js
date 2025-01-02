82e85195a4d5247ab7e1ba2d82736318aa397522
jaisanth123 <jaisanth2006@gmail.com>
2025-01-02 08:55:29 +0530

    forntend initiated

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+
+function App() {
+  return (
+    <>
+    <div className="text-2xl text-white m-10 bg-slate-400 rounded-md">
+      {/* rounded md is like border-radius*/}
+      hello
+
+      
+
+    </div>
+    <button className="btn btn-lg btn-success btn-outline">Accent</button>
+    {/* btn-outline will show only outline while not hovering */}
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,26 @@
+import daisyUIThemes  from "daisyui/src/theming/themes"
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+        themes: ["light",
+          // it will have two themes light and black  here the light theme is default and the black theme has some colors overwritten
+          {
+            black: {
+              ...daisyUIThemes["black"],
+              primary: "rgb(29,155,240)",  //twitter blue color  //! when we use primary in tailwind that will use this color
+              primary: "rgb(24,24,24)",
+            },
+          },
+        ],
+      },
+    
+  
+}
+
