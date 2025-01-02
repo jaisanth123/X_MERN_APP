33a143d53ef3f2a402fbc4f7340ae72c6600390b
jaisanth123 <jaisanth2006@gmail.com>
2025-01-02 19:35:47 +0530

    profile in side bar updated

--- frontend/src/components/common/Sidebar.js
+++ frontend/src/components/common/Sidebar.js
@@ -5,16 +5,12 @@ import { IoNotifications } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { BiLogOut } from "react-icons/bi";
-import {useMutation, useQueryClient} from "@tanstack/react-query"
+import {useMutation, useQuery, useQueryClient} from "@tanstack/react-query"
 import { baseUrl } from "../../constant/url";
 import toast from "react-hot-toast"
 
 const Sidebar = () => {
-  const data = {
-    fullname: "John Doe",
-    username: "johndoe",
-    profileImg: "/avatars/boy1.png",
-  };
+
   
   const queryClient = useQueryClient();
 
@@ -60,6 +56,8 @@ toast.promise(
 )
 }
 
+const {data:authUser} = useQuery({queryKey:["authUser"]})
+
   return (
     <div className="md:flex-[2_2_0] w-18 max-w-52">
       <div className="sticky top-0 left-0 h-screen flex flex-col border-r border-gray-700 w-20 md:w-full">
@@ -88,7 +86,7 @@ toast.promise(
 
           <li className="flex justify-center md:justify-start">
             <Link
-              to={`/profile/${data?.username}`}
+              to={`/profile/${authUser?.username}`}
               className="flex gap-3 items-center hover:bg-stone-900 transition-all rounded-full duration-300 py-2 pl-2 pr-4 max-w-fit cursor-pointer"
             >
               <FaUser className="w-6 h-6" />
@@ -96,15 +94,15 @@ toast.promise(
             </Link>
           </li>
         </ul>
-        {data && (
+        {authUser && (
           <Link
-            to={`/profile/${data.username}`}
+            to={`/profile/${authUser.username}`}
             className="mt-auto mb-10 flex gap-2 items-start transition-all duration-300 hover:bg-[#181818] py-2 px-4 rounded-full"
           >
             <div className="avatar hidden md:inline-flex">
               <div className="w-8 rounded-full">
                 <img
-                  src={data?.profileImg || "/avatar-placeholder.png"}
+                  src={authUser?.profileImg || "/avatar-placeholder.png"}
                   alt="profileImage"
                 />
               </div>
@@ -112,9 +110,9 @@ toast.promise(
             <div className="flex justify-between flex-1">
               <div className="hidden md:block">
                 <p className="text-white font-bold text-sm w-20 truncate">
-                  {data?.fullname}
+                  {authUser?.fullname}
                 </p>
-                <p className="text-slate-500 text-sm">@{data?.username}</p>
+                <p className="text-slate-500 text-sm">@{authUser?.username}</p>
               </div>
               <BiLogOut className="w-5 h-5 cursor-pointer"
               onClick={(e)=>{
