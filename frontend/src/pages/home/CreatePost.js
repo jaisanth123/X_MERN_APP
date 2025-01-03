4ce1f8fe27a83c5d29f1dd54b8921384aa1ac6e1
jaisanth123 <jaisanth2006@gmail.com>
2025-01-03 07:39:01 +0530

    create post profile connected from dummy to fetch

--- frontend/src/pages/home/CreatePost.js
+++ frontend/src/pages/home/CreatePost.js
@@ -2,19 +2,19 @@ import { CiImageOn } from "react-icons/ci";
 import { BsEmojiSmileFill } from "react-icons/bs";
 import { useRef, useState } from "react";
 import { IoCloseSharp } from "react-icons/io5";
+import { useQuery } from "@tanstack/react-query";
 
 const CreatePost = () => {
 	const [text, setText] = useState("");
 	const [img, setImg] = useState(null);
+	const {data : authUser} = useQuery({queryKey: ["authUser"]})
+	
 
 	const imgRef = useRef(null);
 
 	const isPending = false;
 	const isError = false;
 
-	const data = {
-		profileImg: "/avatars/boy1.png",
-	};
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
@@ -33,42 +33,42 @@ const CreatePost = () => {
 	};
 
 	return (
-		<div className='flex p-4 items-start gap-4 border-b border-gray-700'>
+		<div className='flex items-start gap-4 p-4 border-b border-gray-700'>
 			<div className='avatar'>
 				<div className='w-8 rounded-full'>
-					<img src={data.profileImg || "/avatar-placeholder.png"} />
+					<img src={authUser.profileImg || "/avatar-placeholder.png"} />
 				</div>
 			</div>
-			<form className='flex flex-col gap-2 w-full' onSubmit={handleSubmit}>
+			<form className='flex flex-col w-full gap-2' onSubmit={handleSubmit}>
 				<textarea
-					className='textarea w-full p-0 text-lg resize-none border-none focus:outline-none  border-gray-800'
+					className='w-full p-0 text-lg border-gray-800 border-none resize-none textarea focus:outline-none'
 					placeholder='What is happening?!'
 					value={text}
 					onChange={(e) => setText(e.target.value)}
 				/>
 				{img && (
-					<div className='relative w-72 mx-auto'>
+					<div className='relative mx-auto w-72'>
 						<IoCloseSharp
-							className='absolute top-0 right-0 text-white bg-gray-800 rounded-full w-5 h-5 cursor-pointer'
+							className='absolute top-0 right-0 w-5 h-5 text-white bg-gray-800 rounded-full cursor-pointer'
 							onClick={() => {
 								setImg(null);
 								imgRef.current.value = null;
 							}}
 						/>
-						<img src={img} className='w-full mx-auto h-72 object-contain rounded' alt="dummyPost" />
+						<img src={img} className='object-contain w-full mx-auto rounded h-72' alt="dummyPost" />
 					</div>
 				)}
 
-				<div className='flex justify-between border-t py-2 border-t-gray-700'>
-					<div className='flex gap-1 items-center'>
+				<div className='flex justify-between py-2 border-t border-t-gray-700'>
+					<div className='flex items-center gap-1'>
 						<CiImageOn
-							className='fill-primary w-6 h-6 cursor-pointer'
+							className='w-6 h-6 cursor-pointer fill-primary'
 							onClick={() => imgRef.current.click()}
 						/>
-						<BsEmojiSmileFill className='fill-primary w-5 h-5 cursor-pointer' />
+						<BsEmojiSmileFill className='w-5 h-5 cursor-pointer fill-primary' />
 					</div>
 					<input type='file' hidden ref={imgRef} onChange={handleImgChange} />
-					<button className='btn btn-primary rounded-full btn-sm text-white px-4'>
+					<button className='px-4 text-white rounded-full btn btn-primary btn-sm'>
 						{isPending ? "Posting..." : "Post"}
 					</button>
 				</div>
