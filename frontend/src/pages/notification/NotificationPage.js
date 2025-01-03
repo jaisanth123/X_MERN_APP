5e3c7f1f7f652832c5118b2b6c087cf4fa045fb1
jaisanth123 <jaisanth2006@gmail.com>
2025-01-03 14:38:15 +0530

    notifiaction page connected

--- frontend/src/pages/notification/NotificationPage.js
+++ frontend/src/pages/notification/NotificationPage.js
@@ -4,38 +4,78 @@ import LoadingSpinner from "../../components/common/LoadingSpinner";
 import { IoSettingsOutline } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
 import { FaHeart } from "react-icons/fa6";
+import { QueryClient, useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { baseUrl } from "../../constant/url";
+import toast from "react-hot-toast";
 
 const NotificationPage = () => {
-	const isLoading = false;
-	const notifications = [
-		{
-			_id: "1",
-			from: {
-				_id: "1",
-				username: "johndoe",
-				profileImg: "/avatars/boy2.png",
-			},
-			type: "follow",
-		},
-		{
-			_id: "2",
-			from: {
-				_id: "2",
-				username: "janedoe",
-				profileImg: "/avatars/girl1.png",
-			},
-			type: "like",
-		},
-	];
+	
+	const queryClient = useQueryClient()
+	const {data:notifications,isLoading} = useQuery({
+		queryKey:["notification"],
+		queryFn: async () => {
+			try{
+				const res = await fetch (`${baseUrl}/api/notifications`,{
+					method: "GET",
+                    credentials: "include",
+                    headers: {
+                        "Content-Type": "application/json",
+                    }
+				})
+				const data = await res.json();	
+				if(!res.ok){
+                    throw new Error(data.error || "Something went wrong")
+                }
+				return data;
 
-	const deleteNotifications = () => {
-		alert("All notifications deleted");
-	};
+			}
+			catch(err){
+				throw err
+			}
+		}
+	})
+
+const {mutate : deleteNotification} = useMutation({
+	mutationFn: async () =>{
+		try{
+			const res = await fetch (`${baseUrl}/api/notifications`,{
+				method: "DELETE",
+				credentials: "include",
+				headers: {
+					"Content-Type": "application/json",
+				}
+			})
+			const data = await res.json();	
+			if(!res.ok){
+				throw new Error(data.error || "Something went wrong")
+			}
+			return data;
+
+		}
+		catch(err){
+			throw err
+		}
+		
+		
+	},
+	onSuccess:() =>{
+		toast.success("All Notification deleted")
+		queryClient.invalidateQueries({queryKey:["notification"]})
+	},
+	onError: (err) => {
+        toast.error(err.message)
+    }
+})
+
+const deleteNotifications =() =>{
+	deleteNotification()
+	
+}
 
 	return (
 		<>
 			<div className='flex-[4_4_0] border-l border-r border-gray-700 min-h-screen'>
-				<div className='flex justify-between items-center p-4 border-b border-gray-700'>
+				<div className='flex items-center justify-between p-4 border-b border-gray-700'>
 					<p className='font-bold'>Notifications</p>
 					<div className='dropdown '>
 						<div tabIndex={0} role='button' className='m-1'>
@@ -52,16 +92,16 @@ const NotificationPage = () => {
 					</div>
 				</div>
 				{isLoading && (
-					<div className='flex justify-center h-full items-center'>
+					<div className='flex items-center justify-center h-full'>
 						<LoadingSpinner size='lg' />
 					</div>
 				)}
-				{notifications?.length === 0 && <div className='text-center p-4 font-bold'>No notifications 🤔</div>}
+				{notifications?.length === 0 && <div className='p-4 font-bold text-center'>No notifications 🤔</div>}
 				{notifications?.map((notification) => (
 					<div className='border-b border-gray-700' key={notification._id}>
 						<div className='flex gap-2 p-4'>
 							{notification.type === "follow" && <FaUser className='w-7 h-7 text-primary' />}
-							{notification.type === "like" && <FaHeart className='w-7 h-7 text-red-500' />}
+							{notification.type === "like" && <FaHeart className='text-red-500 w-7 h-7' />}
 							<Link to={`/profile/${notification.from.username}`}>
 								<div className='avatar'>
 									<div className='w-8 rounded-full'>
