005066f1f4c60d98bf0ba2b7c2b483b1ea22ce75
jaisanth123 <jaisanth2006@gmail.com>
2025-01-03 15:11:39 +0530

    profile page updated

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -9,7 +9,7 @@ export const getProfile = async (req, res) => {
     const user = await User.findOne({ username });
     if (!user) return res.status(404).json({ message: "error in getprofile" });
 
-    res.status(200).json({ user });
+    res.status(200).json(user );
   } catch (err) {
     console.error(err.message);
     res.status(500).json({ message: "Server Error" });
